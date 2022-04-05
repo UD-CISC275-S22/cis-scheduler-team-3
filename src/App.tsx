45509221e6f7ce5eb132352f134dd9a06c906ea4
jneilson <neilson.john38781@gmail.com>
2022-04-05 15:31:42 -0400

    moved name to header

--- src/App.tsx
+++ src/App.tsx
@@ -7,11 +7,13 @@ function App(): JSX.Element {
             <header className="App-header">
                 UD CISC275 with React Hooks and TypeScript
             </header>
-            <div>John Neilson</div>
             <p>
                 Edit <code>src/App.tsx</code> and save. This page will
                 automatically reload.
             </p>
+            <h1>Group Members:</h1>
+            <h3>Madison Holloway</h3>
+            <h3>John Neilson</h3>
         </div>
     );
 }
