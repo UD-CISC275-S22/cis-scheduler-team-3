d638a26dddbd0da71b9ad00531d2b296ae3f57d4
fleck-sara <sfleck@udel.edu>
2022-04-05 16:31:17 -0400

    added name

--- src/App.tsx
+++ src/App.tsx
@@ -14,6 +14,7 @@ function App(): JSX.Element {
             <h1>Group Members:</h1>
             <h3>Madison Holloway</h3>
             <h3>John Neilson</h3>
+            <h3>Sara Fleck</h3>
         </div>
     );
 }
