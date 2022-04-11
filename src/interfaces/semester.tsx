3ef33a72e80cf281de6c71a1a1693baf74253990
jneilson <neilson.john38781@gmail.com>
2022-04-11 10:25:35 -0400

    Completed base data

--- src/interfaces/semester.tsx
+++ src/interfaces/semester.tsx
@@ -12,15 +12,49 @@ export interface Semester {
     semester_credits: number; // The total number of credits taken during a given semester
 }
 
-export const SEMESTERS = [
+export const BASE_SEMESTERS = [
     {
-        courses: [],
+        courses: [
+            {
+                code: "ENG110",
+                title: "Seminar in Composition",
+                description: "Freshman english class required for all students",
+                course_credits: 3,
+                prerequisites: [],
+                requirement: "none"
+            },
+            {
+                code: "CISC108",
+                title: "Intro to Computer Science I",
+                description: "Basic coding principles",
+                course_credits: 3,
+                prerequisites: [],
+                requirement: "none"
+            }
+        ],
         year: 2021,
         session: "Fall",
         semester_credits: 0
     },
     {
-        courses: [],
+        courses: [
+            {
+                code: "MATH241",
+                title: "Calculus 1",
+                description: "Math Class required for CISC majors",
+                course_credits: 3,
+                prerequisites: [],
+                requirement: "none"
+            },
+            {
+                code: "CISC181",
+                title: "Intro to Computer Science II",
+                description: "More advanced coding principles",
+                course_credits: 3,
+                prerequisites: [],
+                requirement: "none"
+            }
+        ],
         year: 2022,
         session: "Spring",
         semester_credits: 0
