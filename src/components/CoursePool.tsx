837ec8b840f9c2da33dac171cfa120b9f5ca4af7
jneilson <neilson.john38781@gmail.com>
2022-04-14 15:14:17 -0400

    added new component call to course pool

--- src/components/CoursePool.tsx
+++ src/components/CoursePool.tsx
@@ -4,11 +4,13 @@ import { CourseView } from "./CourseView";
 import { ValidateNewCourse } from "./NewCourse";
 import POOL_DATA from "../data/course_pool.json";
 import type { Course } from "../interfaces/course";
+import { CourseMover } from "./CourseMover";
 
 export function CoursePool(): JSX.Element {
     const POOLCOURSES = POOL_DATA as Course[];
     const [newCourse, setNewCourse] = useState<boolean>(false);
     const [poolCourses, setPoolCourses] = useState<Course[]>(POOLCOURSES);
+    const [movecourse, setMoveCourse] = useState<boolean>(false);
 
     function createCourse(newCourse: Course) {
         setPoolCourses([...poolCourses, newCourse]);
@@ -36,6 +38,12 @@ export function CoursePool(): JSX.Element {
                 {poolCourses.map((course: Course) => (
                     <CourseView key={course.code} course={course}></CourseView>
                 ))}
+            </Row>
+            <Row>
+                <Button onClick={() => setMoveCourse(!movecourse)}>
+                    Move Course from Pool
+                </Button>
+                {movecourse ? <CourseMover></CourseMover> : null}
             </Row>
         </Container>
     );
