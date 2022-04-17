9e7e21f17ce47b6dd2259bafd31e3a9caf25bdc1
jneilson <neilson.john38781@gmail.com>
2022-04-17 19:08:31 -0400

    Base Form setup complete

--- src/components/CourseMover.tsx
+++ src/components/CourseMover.tsx
@@ -1,5 +1,32 @@
 import React from "react";
+import { Button, Col, Form, Row } from "react-bootstrap";
 
 export function CourseMover(): JSX.Element {
-    return <div></div>;
+    return (
+        <Col>
+            <Row>
+                <Form.Group>
+                    <Form.Label>Select a Semester to Move From:</Form.Label>
+                    <Form.Select></Form.Select>
+                </Form.Group>
+            </Row>
+            <Row>
+                <Form.Group>
+                    <Form.Label>Select a Course to Move:</Form.Label>
+                    <Form.Select></Form.Select>
+                </Form.Group>
+            </Row>
+            <Row>
+                <Form.Group>
+                    <Form.Label>
+                        Select a Semester or Course Pool to move to:
+                    </Form.Label>
+                    <Form.Select></Form.Select>
+                </Form.Group>
+            </Row>
+            <Row>
+                <Button>Move Course</Button>
+            </Row>
+        </Col>
+    );
 }
