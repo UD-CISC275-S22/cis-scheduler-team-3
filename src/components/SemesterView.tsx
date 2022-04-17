0836a20db085c54e5d8281367b80ba47c953fce2
jneilson <neilson.john38781@gmail.com>
2022-04-17 14:40:10 -0400

    completed insert-course-semester still need tests

--- src/components/SemesterView.tsx
+++ src/components/SemesterView.tsx
@@ -1,24 +1,47 @@
-import React from "react";
-import { Container, Row, Col } from "react-bootstrap";
+import React, { useState } from "react";
+import { Container, Row, Col, Button } from "react-bootstrap";
 import { Semester } from "../interfaces/semester";
 import { CourseList } from "./CourseList";
+import { ValidateNewCourse } from "./NewCourse";
+import { Course } from "../interfaces/course";
 
 export function SemesterView({
     semester
 }: {
     semester: Semester;
 }): JSX.Element {
+    const [newCourse, setNewCourse] = useState<boolean>(false);
+    const [semesterCourses, setSemesterCourses] = useState<Course[]>(
+        semester.courses
+    );
+
+    function createCourse(newCourse: Course) {
+        setSemesterCourses([...semesterCourses, newCourse]);
+    }
+
     return (
         <Container data-testid="Semester">
             <Row>
                 <Col>
-                    <h3 data-testid="Semester_Title">
-                        {semester.session}:{semester.year}
-                    </h3>
-                    <h4 data-testid="Semester_Credits">
-                        Total Credits: {semester.semester_credits}
-                    </h4>
-                    <CourseList courses={semester.courses}></CourseList>
+                    <Row>
+                        <h3 data-testid="Semester_Title">
+                            {semester.session}:{semester.year}
+                        </h3>
+                        <h4 data-testid="Semester_Credits">
+                            Total Credits: {semester.semester_credits}
+                        </h4>
+                        <Button onClick={() => setNewCourse(!newCourse)}>
+                            Add Course to Semester
+                        </Button>
+                    </Row>
+                    <Row>
+                        {newCourse ? (
+                            <ValidateNewCourse
+                                createCourse={createCourse}
+                            ></ValidateNewCourse>
+                        ) : null}
+                    </Row>
+                    <CourseList courses={semesterCourses}></CourseList>
                 </Col>
             </Row>
         </Container>
