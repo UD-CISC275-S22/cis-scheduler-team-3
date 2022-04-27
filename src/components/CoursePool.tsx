46fbff208ef86d361e6d4849d37da2db9e293f9c
jneilson <neilson.john38781@gmail.com>
2022-04-27 14:48:31 -0400

    restored course pool to intended location and functionality

--- src/components/CoursePool.tsx
+++ src/components/CoursePool.tsx
@@ -1,16 +1,16 @@
 import React, { useState } from "react";
 import { Button, Container, Row } from "react-bootstrap";
-import { CoursePoolTable } from "./CoursePoolTable";
+import { CourseView } from "./CourseView";
 import { ValidateNewCourse } from "./NewCourse";
 import type { Course } from "../interfaces/course";
-import { DegreePlan } from "../interfaces/degreeplan";
-import POOL_DATA from "../data/course_catalog.json";
 
-export function CoursePool({ plans }: { plans: DegreePlan[] }): JSX.Element {
-    const POOLCOURSES = POOL_DATA as Course[];
-    const DEGREEPLANS = [...plans];
+export function CoursePool({
+    plan_pool
+}: {
+    plan_pool: Course[];
+}): JSX.Element {
     const [newCourse, setNewCourse] = useState<boolean>(false);
-    const [poolCourses, setPoolCourses] = useState<Course[]>(POOLCOURSES);
+    const [poolCourses, setPoolCourses] = useState<Course[]>(plan_pool);
 
     function createCourse(newCourse: Course) {
         setPoolCourses([...poolCourses, newCourse]);
@@ -20,10 +20,7 @@ export function CoursePool({ plans }: { plans: DegreePlan[] }): JSX.Element {
         setNewCourse(!newCourse);
     }
     return (
-        <Container
-            className="course-pool"
-            style={{ overflowY: "scroll", height: "400px" }}
-        >
+        <Container className="course-pool">
             <Row>
                 <h5>Course Pool: </h5>
                 <Button
@@ -31,7 +28,7 @@ export function CoursePool({ plans }: { plans: DegreePlan[] }): JSX.Element {
                     className="Buttons"
                     data-testid="add-course-btn"
                 >
-                    {newCourse ? "Cancel" : "Add Course to Pool"}
+                    Add Course to Pool
                 </Button>
             </Row>
             <Row>
@@ -41,13 +38,9 @@ export function CoursePool({ plans }: { plans: DegreePlan[] }): JSX.Element {
                     ></ValidateNewCourse>
                 ) : null}
             </Row>
-            <Row>
+            <Row lg={6} id="course-row" data-testid="course-pool">
                 {poolCourses.map((course: Course) => (
-                    <CoursePoolTable
-                        key={course.code}
-                        course={course}
-                        plans={DEGREEPLANS}
-                    ></CoursePoolTable>
+                    <CourseView key={course.code} course={course}></CourseView>
                 ))}
             </Row>
         </Container>
