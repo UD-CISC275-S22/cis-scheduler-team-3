f026f46d1a3748e0c06ed38e7784bc593bdb8f32
fleck-sara <sfleck@udel.edu>
2022-05-09 23:57:47 -0400

    make sure all tests are still passing

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -2,8 +2,8 @@ import React from "react";
 import { render, screen } from "@testing-library/react";
 import App from "./App";
 
-test("renders the course name somewhere", () => {
+test("renders the welcome message", () => {
     render(<App />);
-    const linkElement = screen.getByText(/UD CIS Scheduler/i);
+    const linkElement = screen.getByText(/Hello!/i);
     expect(linkElement).toBeInTheDocument();
 });
